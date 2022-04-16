f9bbb064be292f2ea1da23d2b2fcabaacfdca73a
LJH0716 <jhmk0805@naver.com>
2022-04-16 22:21:34 +0900

    2022-04-16 7

--- src/com/javaex/practice/Ex09.java
+++ src/com/javaex/practice/Ex09.java
@@ -14,7 +14,6 @@ public class Ex09 {
 		System.out.print("두번째 숫자: ");
 		int num2 = sc.nextInt();
 		
-		
 		if(num1>num2 && num1%num2==0 ) {
 			System.out.println( num2 + " 는(은) "+num1+ " 의 약수입니다.");
 		}
@@ -28,6 +27,7 @@ public class Ex09 {
 		else {
 			System.out.println( num1 + " 는(은) " +num2+ " 의 약수가 아닙니다.");
 		}
+		
 		
 		
 		sc.close();
--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -1,10 +1,26 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("알파벳 1글자(소문자)를 입력하세요");
+		
+		System.out.print("알파벳: ");
+		String word = sc.nextLine();
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }

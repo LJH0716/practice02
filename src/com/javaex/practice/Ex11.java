8dbb623bb766d430e816b8d6b57f6fac0a8c6ac2
LJH0716 <jhmk0805@naver.com>
2022-04-17 22:44:26 +0900

    2022-04-17 1

--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -8,13 +8,25 @@ public class Ex11 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("알파벳 1글자(소문자)를 입력하세요");
-		
+		System.out.println("알파벳 1글자(소문자)를 입력하세요.");
 		System.out.print("알파벳: ");
-		String word = sc.nextLine();
+		String letter = sc.nextLine();
 		
+		switch(letter) {
+			case "a":
+			case "e":
+			case "i":
+			case "o":
+			case "u":
+				System.out.println("모음입니다.");
+				break;
+				
+			default:
+				System.out.println("자음입니다.");
+				break;
+				
 		
-		
+		}
 		
 		
 		
